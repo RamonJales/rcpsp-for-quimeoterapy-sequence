3de0cb129d5bff35bf079fe0d37fdd50b115dc93
Jeremiasp7 <jeremiasp2004@gmail.com>
2025-10-13 23:05:32 -0300

    adding tests choice

--- main.py
+++ main.py
@@ -2,25 +2,31 @@ from algorithm import gerar_cronograma
 from view_calendar import imprimir_cronograma
 from parse_sm import parse_sm_file
 
-for i in [5, 25, 40, 50]:
 
-    caminho_do_arquivo = f"instances/instancias_geradas/folfiri_{i}_pacientes.sm"
+n_pacientes = input("Digite o número de pacientes (5, 25, 40, 50): ")
 
-    atividades, precedencias, recursos = parse_sm_file(caminho_do_arquivo)
+if n_pacientes not in ['5', '25', '40', '50']:
+    print("Número inválido. Por favor, escolha entre 5, 25, 40 ou 50.")
+    exit(1)
 
-    incompatibilidades = [
-        (0, 1),
-        (2, 3)  
-    ]
+n = int(n_pacientes)
+caminho_do_arquivo = f"instances/instancias_geradas/folfiri_{n}_pacientes.sm"
 
-    cronograma = gerar_cronograma(atividades, recursos, precedencias, incompatibilidades)
+atividades, precedencias, recursos = parse_sm_file(caminho_do_arquivo)
+
+incompatibilidades = [
+    (0, 1),
+    (2, 3)  
+]
+
+cronograma = gerar_cronograma(atividades, recursos, precedencias, incompatibilidades)
     
-    print("\n" + "="*80)
-    print(f"      RELATÓRIO DETALHADO DO CRONOGRAMA PARA {i} PACIENTES")
-    print("="*80)
+print("\n" + "="*80)
+print(f"      RELATÓRIO DETALHADO DO CRONOGRAMA PARA {n} PACIENTES")
+print("="*80)
 
-    imprimir_cronograma(cronograma, atividades, precedencias, num_pacientes=i)
-    print("\n\n")
+imprimir_cronograma(cronograma, atividades, precedencias, num_pacientes=n)
+print("\n\n")
 
 
 
